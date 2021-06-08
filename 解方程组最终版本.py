3226a4833d1d49a5b1e475229c202e3fd6c349dc
LuboAB <83505636+LuboAB@users.noreply.github.com>
2021-06-08 15:23:16 +0800

    Add files via upload
    输入点的坐标解直线交点

---
+++ 解方程组最终版本.py
@@ -0,0 +1,24 @@
+x1=float(input("x1\n"))
+y1=float(input("y1\n"))
+x2=float(input("x2\n"))
+y2=float(input("y2\n"))
+x3=float(input("x3\n"))
+y3=float(input("y3\n"))
+x4=float(input("x4\n"))
+y4=float(input("y4\n"))
+
+k1=(y2-y1)*1.0/(x2-x1)#计算k1,由于点均为整数，需要进行浮点数转化
+b1=y1*1.0-x1*k1*1.0#整型转浮点型是关键
+if (x4-x3)==0:#L2直线斜率不存在操作
+    k2=None
+    b2=0
+else:
+    k2=(y4-y3)*1.0/(x4-x3)#斜率存在操作
+    b2=y3*1.0-x3*k2*1.0
+if k2==None:
+    x=x3
+else:
+    x=(b2-b1)*1.0/(k1-k2)
+y=k1*x*1.0+b1*1.0
+print("交点为")
+print(x,y)
